fd19156fe99572c0accebcfc940e8e4da62da0ad
Leah <leah.jl@proton.me>
2023-08-28 11:03:17 +0200

    portfolio iframe included

---
+++ leah.php
@@ -0,0 +1,15 @@
+<?php include("head.php"); ?>
+
+<body>
+    <?php include("header.php"); ?>
+    <?php include("nav.php"); ?>
+    <main>
+        <div id="i-frame-container">
+            <iframe src="https://leah-joannes-laplace.netlify.app/" width="100%" height="800"></iframe>
+        </div>
+
+    </main>
+</body>
+<?php include("script.php") ?>
+
+</html>
