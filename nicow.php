8aad1390a1df9dab890970e594b19000a1f8eb71
ElRecipiente <nicolas.droux.2b@gmail.com>
2023-08-28 13:22:36 +0200

    fix

--- nicow.php
+++ nicow.php
@@ -9,11 +9,12 @@
         <h1>Nicow avec un W</h1>
         <p>Adepte de JDR et jeux vidéo en tout genre, âgé de 31 ans, autrefois perdu (longtemps) dans les études, et
             après avoir testé pas mal de p'tits boulots, je me suis finalement
-            reconverti aujourd'hui au développement web. Grosse appétence pour le front, moins pour le back mais
+            reconverti au développement web. Grosse appétence pour le front, moins pour le back mais
             quand même curieux de
             <span class="italic bold">comment que ça marche ce truc</span>, j'aime bien faire des trucs funs, si
             possible un peu jolies,
-            même si parfois ce n'est ni l'un ni l'autre... <a class="italic bold" href="https://thebigrecipient-portfolio.netlify.app/">On peut trouver mon portfolio ici.</a>
+            même si parfois ce n'est ni l'un ni l'autre... <a class="italic bold"
+                href="https://thebigrecipient-portfolio.netlify.app/">On peut trouver mon portfolio ici.</a>
         </p>
         <iframe id="portfolio" title="portfolio" src="https://thebigrecipient-portfolio.netlify.app/"></iframe>
     </main>
